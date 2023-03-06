8770170319f4ed00c5a2d3f9a4c7aafa7ef0a362
Zhiqiang Li <stulzq@qq.com>
2023-03-07 01:52:37 +0800

    feat: add ut

---
+++ TestLib/MyClass.cs
@@ -0,0 +1,30 @@
+﻿namespace TestLib
+{
+    /// <summary>
+    /// Represents a class with methods to perform addition and subtraction operations.
+    /// </summary>
+    public class MyClass
+    {
+        /// <summary>
+        /// Adds two integers and returns the result.
+        /// </summary>
+        /// <param name="a">The first integer to add.</param>
+        /// <param name="b">The second integer to add.</param>
+        /// <returns>The sum of the two integers.</returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// Subtracts one integer from another and returns the result.
+        /// </summary>
+        /// <param name="a">The integer to subtract from (the minuend).</param>
+        /// <param name="b">The integer to subtract (the subtrahend).</param>
+        /// <returns>The difference between the two integers.</returns>
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
