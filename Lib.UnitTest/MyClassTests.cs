2a44429a2f807192a50b06e90ea84fd111ccea1a
Zhiqiang Li <stulzq@qq.com>
2023-03-07 21:21:09 +0800

    test

--- Lib.UnitTest/MyClassTests.cs
+++ Lib.UnitTest/MyClassTests.cs
@@ -17,17 +17,17 @@ namespace Lib.UnitTest
             Assert.Equal(5, result);
         }
 
-        // [Fact]
-        // public void TestSubtract()
-        // {
-        //     // Arrange
-        //     MyClass myClass = new MyClass();
-        //
-        //     // Act
-        //     int result = myClass.Subtract(3, 2);
-        //
-        //     // Assert
-        //     Assert.Equal(1, result);
-        // }
+        [Fact]
+        public void TestSubtract()
+        {
+            // Arrange
+            MyClass myClass = new MyClass();
+        
+            // Act
+            int result = myClass.Subtract(3, 2);
+        
+            // Assert
+            Assert.Equal(1, result);
+        }
     }
 }
